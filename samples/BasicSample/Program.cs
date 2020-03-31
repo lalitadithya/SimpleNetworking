a681c85d3839f3da73433ebd1262721a327bde30
lalitadithya1996@hotmail.com <lalitadithya1996@hotmail.com>
2020-03-31 20:11:51 +0530

    Added sample

--- samples/BasicSample/Program.cs
+++ samples/BasicSample/Program.cs
@@ -1,12 +1,74 @@
-﻿using System;
+﻿using SimpleNetworking.Client;
+using SimpleNetworking.Serializer;
+using SimpleNetworking.Server;
+using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace BasicSample
 {
+    public class MyPacket
+    {
+        public string Data { get; set; }
+    }
+
     class Program
     {
+        static IInsecureClient client;
+        static string type;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CancellationTokenSource cts = new CancellationTokenSource();
+            Console.WriteLine("1 -> Client");
+            Console.WriteLine("2 -> Server");
+            int choice = int.Parse(Console.ReadLine());
+            switch(choice)
+            {
+                case 1:
+                    type = "client";
+                    client = new InsecureClient();
+                    client.OnPacketReceived += Client_OnPacketReceived;
+                    client.Connect("localhost", 9000, new JsonSerializer());
+                    Console.WriteLine("Connect success");
+                    break;
+                case 2:
+                    type = "sever";
+                    InsecureServer server = new InsecureServer();
+                    server.OnClientConnected += Server_OnClientConnected;
+                    server.StartListening(IPAddress.Any, 9000, new JsonSerializer(), cts.Token);
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+
+        private static void Client_OnPacketReceived(object data)
+        {
+            Task.Run(() =>
+            {
+                MyPacket packet = (MyPacket)data;
+                Console.WriteLine("Got: " + packet.Data);
+                client.SendData(new MyPacket
+                {
+                    Data = packet.Data.Contains("Ping") ? $"{type}: Pong" : $"{type}: Ping"
+                });
+            });
+        }
+
+        private static void Server_OnClientConnected(IInsecureClient client1)
+        {
+            Console.WriteLine("Client connected");
+            client = client1;
+            Task.Run(() =>
+            {
+                client.OnPacketReceived += Client_OnPacketReceived;
+                client.SendData(new MyPacket
+                {
+                    Data = $"{type}: Ping"
+                });
+            });
         }
     }
 }
