c186b4240298c4c190a0115429bd13be2c3d1aff
lalitadithya1996@hotmail.com <lalitadithya1996@hotmail.com>
2020-03-31 18:14:30 +0530

    Added sample project

---
+++ samples/BasicSample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BasicSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
