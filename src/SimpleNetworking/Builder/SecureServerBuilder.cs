b90777602f4ab67b2bd0f842fbe383a2e77a7fb9
lalitadithya1996@hotmail.com <lalitadithya1996@hotmail.com>
2020-04-10 18:16:26 +0530

    Added SecureServerBuilder

---
+++ src/SimpleNetworking/Builder/SecureServerBuilder.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.Extensions.Logging;
+using SimpleNetworking.IdempotencyService;
+using SimpleNetworking.Models;
+using SimpleNetworking.OrderingService;
+using SimpleNetworking.SequenceGenerator;
+using SimpleNetworking.Serializer;
+using SimpleNetworking.Server;
+using System;
+using System.Collections.Generic;
+using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+
+namespace SimpleNetworking.Builder
+{
+    public class SecureServerBuilder
+    {
+        private int millisecondsIntervalForPacketResend = 60 * 1000;
+
+        private ILoggerFactory loggerFactory;
+        private ISerializer serializer = new JsonSerializer();
+        private CancellationToken cancellationToken = CancellationToken.None;
+        private IOrderingService orderingService = new SimplePacketOrderingService();
+        private ISendIdempotencyService<Guid, Packet> sendIdempotencyService = new SendIdempotencyService<Guid, Packet>(maximumPacketBacklog);
+        private IReceiveIdempotencyService<string> receiveIdempotencyService = new ReceiveIdempotencyService<string>(expiryTime);
+        private ISequenceGenerator delaySequenceGenerator = new ExponentialSequenceGenerator(maximumBackoffTime);
+
+        private static readonly int maximumPacketBacklog = 1000;
+        private static readonly int expiryTime = 600000;
+        private static readonly int maximumBackoffTime = 60 * 1000;
+
+        public SecureServerBuilder WithLogger(ILoggerFactory loggerFactory)
+        {
+            this.loggerFactory = loggerFactory;
+            return this;
+        }
+
+        public SecureServerBuilder WithSerilizer(ISerializer serializer)
+        {
+            this.serializer = serializer;
+            return this;
+        }
+
+        public SecureServerBuilder WithCancellationToken(CancellationToken cancellationToken)
+        {
+            this.cancellationToken = cancellationToken;
+            return this;
+        }
+
+        public SecureServerBuilder WithPacketOrderingService(IOrderingService orderingService)
+        {
+            this.orderingService = orderingService;
+            return this;
+        }
+
+        public SecureServerBuilder WithSendIdempotencyService(ISendIdempotencyService<Guid, Packet> sendIdempotencyService)
+        {
+            this.sendIdempotencyService = sendIdempotencyService;
+            return this;
+        }
+
+        public SecureServerBuilder WithReceiveIdempotencyService(IReceiveIdempotencyService<string> receiveIdempotencyService)
+        {
+            this.receiveIdempotencyService = receiveIdempotencyService;
+            return this;
+        }
+
+        public SecureServerBuilder WithSequenceGenerator(ISequenceGenerator delaySequenceGenerator)
+        {
+            this.delaySequenceGenerator = delaySequenceGenerator;
+            return this;
+        }
+
+        public SecureServerBuilder WithIntervalForPacketResend(TimeSpan packetResendInterval)
+        {
+            this.millisecondsIntervalForPacketResend = (int)packetResendInterval.TotalMilliseconds;
+            return this;
+        }
+
+        public SecureServer Build(X509Certificate serverCertificate, bool clientCertificateRequired = false, 
+            SslProtocols sslProtocols = SslProtocols.Tls13, ClientCertificateValidationCallback clientCertificateValidationCallback = null)
+        {
+            return new SecureServer(loggerFactory, serializer, orderingService, cancellationToken, sendIdempotencyService,
+                receiveIdempotencyService, delaySequenceGenerator, millisecondsIntervalForPacketResend, serverCertificate, clientCertificateRequired,
+                sslProtocols, clientCertificateValidationCallback);
+        }
+    }
+}
